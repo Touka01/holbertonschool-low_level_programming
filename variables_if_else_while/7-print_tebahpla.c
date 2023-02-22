dc1bfee12b68933ca6777f6ccba376d1b40d0574
Touka01 <ahmedtoukebri0@gmail.com>
2023-02-22 12:28:43 -0800

    fix

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	putchar(alphabet);
+	alpha--;
+	putchar('\n');
+	return (0);
+}
