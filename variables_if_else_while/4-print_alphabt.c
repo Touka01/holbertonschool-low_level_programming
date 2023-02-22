8023a0206562586c8afe7424668503d873858c4b
Touka01 <ahmedtoukebri0@gmail.com>
2023-02-22 03:09:58 -0800

    fix

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	putchar(lowercase);
+	putchar('\n')
+	grep - v "qe"
+}
