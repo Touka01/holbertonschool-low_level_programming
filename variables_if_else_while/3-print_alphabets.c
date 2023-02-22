2d9ba121f465170efe389e6ae39319a7a7c23348
Touka01 <ahmedtoukebri0@gmail.com>
2023-02-22 02:59:21 -0800

    fix

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -11,8 +11,8 @@ int main(void)
 	char uppercase;
 
 	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
-	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
 	putchar(lowercase);
+	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
 	putchar(uppercase);
 	putchar('\n');
 	return (0);
