a1e5d98eb35f340e155ac3b3869dae94d8c6d86c
Touka01 <ahmedtoukebri0@gmail.com>
2023-02-23 04:23:09 -0800

    alphabet with _ptchar

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void);
+{
+	char  alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	_putchar(alphabet);
+	_putchar('\n');
+	return (0);
+}
