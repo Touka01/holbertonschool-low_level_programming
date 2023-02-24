3c365dd0198813b3a0bb2237d76e5115483eb2b6
Touka01 <ahmedtoukebri0@gmail.com>
2023-02-24 02:38:13 -0800

    fixx

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_alphabet - prints lowercase alphabet
  * Return: 0
---
+++ functions_nested_loops/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
