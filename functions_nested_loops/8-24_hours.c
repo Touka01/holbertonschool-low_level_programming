52c72f086e3eb56f7b8b66a7c47c170929acf8f9
Touka01 <ahmedtoukebri0@gmail.com>
2023-02-28 01:03:45 -0800

    24hours

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* jack_bauer - Entry point
+*
+* Return: value
+*/
+
+void jack_bauer(void)
+{
+	int hour, min;
+
+	for (hour = 0; hour < 24; hour++)
+		for (min = 0 ; min < 60; min++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+}
