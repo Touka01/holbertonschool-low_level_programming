2363683ebbb47c1ddb92a4b1e9e391878474deea
Touka01 <ahmedtoukebri0@gmail.com>
2023-03-05 03:56:44 -0800

    rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	s--;
+	for (o = length; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
