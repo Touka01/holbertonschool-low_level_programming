978ca7c1d3b244d93d79466758fde177eb1c5dbd
Touka01 <ahmedtoukebri0@gmail.com>
2023-03-07 02:31:28 -0800

    rev

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,9 +10,11 @@ void rev_string(char *s)
 {
 	int len = strlen(s);
 	int i;
-	for(i = 0; i  < len / 2; i++)
+
+	for (i = 0; i  < len / 2; i++)
 	{
 	char temp = s[i];
+
 	s[i] = s[len - i - 1];
 	s[len - i - 1] = temp;
 	}
