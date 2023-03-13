fe47513a4e43d10c34fa26d9a6fa2312f7bf04af
Touka01 <ahmedtoukebri0@gmail.com>
2023-03-13 05:52:34 -0700

    rev

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - print string in reverse
+* @s: pointer to string
+* Return
+*
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*(s));
+}
