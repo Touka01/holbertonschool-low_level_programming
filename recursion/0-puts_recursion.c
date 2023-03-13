42f8c3bc7e1372724f117438d445e5d02767ab36
Touka01 <ahmedtoukebri0@gmail.com>
2023-03-13 05:50:20 -0700

    puts

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _puts_recursion - function that prints a string
+* @s: pointer to string
+* Return: string
+*/
+
+void _puts_recursion(char *s)
+
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[0]);
+	_puts_recursion(s + 1);
+}
