58ca9c998a3f173bd3d3b31b035ee7c85076085f
Touka01 <ahmedtoukebri0@gmail.com>
2023-03-15 05:12:15 -0700

    args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* Main - prints the number of argments passed into it
+* @argv: an array containing the programm command line arguments
+* @argv: N of command line arguments
+* Return: (0 - Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	(void)argv;
+	return (0);
+}
