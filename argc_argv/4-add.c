ea1317f1e6fc12d8cdddbcef4fdfea74601c5174
Touka01 <ahmedtoukebri0@gmail.com>
2023-03-16 07:48:05 -0700

    add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -3,24 +3,31 @@
 #include <ctype.h>
 
 /**
-* main - program that adds positive numbers
-* @argc: The number of command line arguments
-* @argv: An array containing the program command line arguments
-* Return: Always 0 (Success)
-*/
-
-int main(int argc, char *argv[]) {
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if successful, 1 if error
+ */
+int main(int argc, char *argv[])
+{
     int sum = 0;
     int i, j;
-    for (i = 1; i < argc; i++) {
-        for (j = 0; argv[i][j]; j++) {
-            if (!isdigit(argv[i][j])) {
+
+    for (i = 1; i < argc; i++)
+    {
+        for (j = 0; argv[i][j]; j++)
+        {
+            if (!isdigit(argv[i][j]))
+            {
                 puts("Error");
-                return 1;
+                return (1);
             }
         }
+
         sum += atoi(argv[i]);
     }
+
     printf("%d\n", sum);
-    return 0;
+    return (0);
 }
+
