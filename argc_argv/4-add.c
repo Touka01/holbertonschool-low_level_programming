c3c31c971b0bece060f2b836f978b4b691d26636
Touka01 <ahmedtoukebri0@gmail.com>
2023-03-16 07:59:25 -0700

    add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -6,28 +6,28 @@
  * main - adds positive numbers
  * @argc: argument count
  * @argv: argument vector
+ *
  * Return: 0 if successful, 1 if error
  */
 int main(int argc, char *argv[])
 {
-    int sum = 0;
-    int i, j;
+	int sum =0;
+	int i, j;
 
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j]; j++)
-        {
-            if (!isdigit(argv[i][j]))
-            {
-                puts("Error");
-                return (1);
-            }
-        }
+	for (i=1; i< argc; i++)
+	{
+		for (j =0; argv[i][j]; j++))
+	{
+		if (!isdigit(argv[i][j]))
+		{
+			puts("Error");
+			return (1);
+		}
+	}
 
-        sum += atoi(argv[i]);
-    }
+		sum += atoi(argv[i]);
+	}
 
-    printf("%d\n", sum);
-    return (0);
+	printf("%d\n", sum);
+	return (0);
 }
-
