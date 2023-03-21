1255a0dd741774078b62f3ab440266627235640c
Touka01 <ahmedtoukebri0@gmail.com>
2023-03-21 03:14:08 -0700

    sqrt recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,7 +8,25 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
+	return (find(0, n));
+}
+
+/**
+* find - finds sqrt of a number
+* @i: int to check with
+* @n: number
+* Return: int
+*/
+
+int find(int i, int n)
+{
+	if (n > 0)
+	{
+		n -= (1 + 2 * i);
+		return (find(i + 1, n));
+	}
+	else if (n == 0)
+		return (i);
+	else
 		return (-1);
-	return (_sqrt_recursion(n));
 }
