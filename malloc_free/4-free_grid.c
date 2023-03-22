0123e7edd7c58cfe739e5b91a7ccf6df8c33616e
Ahmed Toukebri <35146059+Touka01@users.noreply.github.com>
2023-03-22 13:02:48 +0000

    fix

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -8,13 +8,3 @@
  * @grid: pointer to 2D array
  * @height: height of array
  */
-
-void free_grid(int **grid, int height)
-{
-    int i;
-
-    for (i = 0; i < height; i++)
-        free(grid[i]);
-
-    free(grid);
-}
