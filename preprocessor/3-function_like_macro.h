27b475ae5ffd0968dca8fa9941a4af221a2e3fe9
Ahmed Toukebri <35146059+Touka01@users.noreply.github.com>
2023-03-27 15:16:32 +0000

    function

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,6 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
-
-#define ABS(x) (((x) < 0) ? (-x) : (x))
-
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
 #endif
