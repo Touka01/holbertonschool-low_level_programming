b0df64ad7a9a1dcbe01353a870bedc3ab5fbeef3
Ahmed Toukebri <35146059+Touka01@users.noreply.github.com>
2023-03-27 16:38:14 +0000

    fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -8,11 +8,11 @@
  * @str: string
  * Return: length
  */
-int len(const char *str)
+int len(char *str)
 {
     int i;
 
-    for (i = 0; str[i]; i++)
+    for (i = 0; *(str + i); i++)
         ;
     return (i);
 }
@@ -27,17 +27,15 @@ int len(const char *str)
 dog_t *new_dog(char *name, float age, char *owner)
 {
     dog_t *dog1;
-    char *copy_of_name = NULL;
-    char *copy_of_owner = NULL;
+    char *copy_of_name;
+    char *copy_of_owner;
 
-    dog1 = malloc(sizeof(dog_t)); /* validate if dog1 initiated correctly */
+    dog1 = malloc(sizeof(dog_t));
     if (dog1 == NULL)
         return (NULL);
 
     dog1->age = age;
 
-    /* make copies of struct members and validate, else free on error */
-    /* set values of struct members to copies of arguments or set to NULL */
     if (name != NULL)
     {
         copy_of_name = malloc(len(name) + 1);
@@ -46,12 +44,7 @@ dog_t *new_dog(char *name, float age, char *owner)
             free(dog1);
             return (NULL);
         }
-        if (strcpy_s(copy_of_name, len(name) + 1, name) != 0)
-        {
-            free(copy_of_name);
-            free(dog1);
-            return (NULL);
-        }
+        strcpy(copy_of_name, name);
         dog1->name = copy_of_name;
     }
     else
@@ -66,13 +59,7 @@ dog_t *new_dog(char *name, float age, char *owner)
             free(dog1);
             return (NULL);
         }
-        if (strcpy_s(copy_of_owner, len(owner) + 1, owner) != 0)
-        {
-            free(copy_of_name);
-            free(copy_of_owner);
-            free(dog1);
-            return (NULL);
-        }
+        strcpy(copy_of_owner, owner);
         dog1->owner = copy_of_owner;
     }
     else
