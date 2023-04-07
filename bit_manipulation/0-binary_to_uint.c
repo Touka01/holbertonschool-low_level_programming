8c0cef267ff9f1c7611ae4171666355b435e9db7
Ahmed Toukebri <35146059+Touka01@users.noreply.github.com>
2023-04-07 01:56:29 +0000

    fix

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -7,31 +7,30 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-	 if (b == NULL)
-		  return (0);
+    if (!b)
+        return (0);
 
-	 int len = 0;
-	 while (b[len] != '\0' && b[len] != 'H')
-		  len++;
+    unsigned int len = 0, sum = 0;
+    while (b[len] && b[len] != 'H')
+        len++;
 
-	 unsigned int sum = 0;
-	 for (int i = 0; i < len; i++)
-	 {
-		  if (b[i] == '0' || b[i] == '1')
-				sum += (b[i] - '0') * (1u << (len - i - 1));
-		  else
-				return (0);
-	 }
+    for (unsigned int i = 0; i < len; i++)
+    {
+        if (b[i] == '0')
+            continue;
+        if (b[i] == '1')
+            sum |= 1u << (len - i - 1);
+        else
+            return (0);
+    }
 
-	 if (b[len] == 'H')
-	 {
-		  for (int i = len + 1; b[i] != '\0'; i++)
-		  {
-				if (b[i] != '0')
-					 return (0);
-		  }
-		  sum *= 10u;
-	 }
+    if (b[len] == 'H')
+    {
+        for (unsigned int i = len + 1; b[i]; i++)
+            if (b[i] != '0')
+                return (0);
+        sum *= 10u;
+    }
 
-	 return (sum);
+    return (sum);
 }
