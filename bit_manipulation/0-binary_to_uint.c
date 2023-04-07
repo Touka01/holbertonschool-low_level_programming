6d1a8a3cec018f949abd1b17298aeec3a362b70f
Ahmed Toukebri <35146059+Touka01@users.noreply.github.com>
2023-04-07 01:30:40 +0000

    fix

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -7,31 +7,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-
-	int len = 0, i;
 	unsigned int sum = 0;
 
 	if (b == NULL)
-		return (sum);
-
-	/* find string length */
-	while (b[len] != '\0')
-		len++;
-	len -= 1;
-
-	/* iterate string and if '1' then multiply by power of 2 */
-	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
-	i = 0;
-	while (b[i])
+		return 0;
+		
+	for (int i = 0; b[i] != '\0'; i++)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
+		if (b[i] != '0' && b[i] != '1')
+		return 0;
 
+		sum <<= 1;
 		if (b[i] == '1')
-			sum += (1 * (1 << len));
-		i++;
-		len--;
+		sum += 1;
 	}
 
-	return (sum);
+	return sum;
 }
