1d9d73202592760a198740316f857b8540aaca75
Ahmed Toukebri <35146059+Touka01@users.noreply.github.com>
2023-04-07 01:33:16 +0000

    fix

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,35 @@
 #include "main.h"
 
-/**
- * binary_to_uint - convert binary to unsigned int
- * @b: binary
- * Return: unsigned int
- */
-unsigned int binary_to_uint(const char *b)
-{
-	unsigned int sum = 0;
+/*
+*binary_to_uint - convert binary to unsigned int
+*@b: binary
+* Return: unsigned int
+*/
 
-	if (b == NULL)
-		return 0;
-		
-	for (int i = 0; b[i] != '\0'; i++)
-	{
-		if (b[i] != '0' && b[i] != '1')
-		return 0;
-
-		sum <<= 1;
-		if (b[i] == '1')
-		sum += 1;
-	}
-
-	return sum;
-}
+unsigned int binary_to_uint(const char b)
+ {
+ 
+ 	int len = 0, pow = 1;
+ 	unsigned int sum = 0, error = 0;
+ 
+ 	if (b == NULL)
+ 		return (error);
+ 
+ 	while (b[len] != '\0') // find string length
+ 		len++;
+ 	len -= 1;
+ 
+ 	while (len >= 0) // iterate from back of string
+ 	{
+ 		if ((b[len] != '0') && (b[len] != '1'))
+ 			return (error);
+ 
+ 		if (b[len] == '1') // add appropriate power of 2 if '1'
+ 			sum += pow;
+ 
+ 		pow = 2; // update power of 2
+ 		len--;
+ 	}
+ 
+ 	return (sum);
+ }
