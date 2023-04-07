5b9b12730eb0f2b0ad23002e5529a3c3ba6b7349
Ahmed Toukebri <35146059+Touka01@users.noreply.github.com>
2023-04-07 01:42:28 +0000

    fix

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -8,31 +8,43 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-
 	int len = 0, i;
 	unsigned int sum = 0;
-
+		
 	if (b == NULL)
-		return (sum);
+		return 0;
 
-	/* find string length */
-	while (b[len] != '\0')
+	// Find string length, ignoring any 'H' characters
+	while (b[len] != '\0' && b[len] != 'H')
 		len++;
-	len -= 1;
 
-	/* iterate string and if '1' then multiply by power of 2 */
-	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
+	// Iterate through string, handling each character
 	i = 0;
-	while (b[i])
-	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
-
-		if (b[i] == '1')
-			sum += (1 * (1 << len));
+	while (i < len) {
+		if (b[i] == '0' || b[i] == '1') {
+			sum += (b[i] - '0') * (1u << (len - i - 1));
+		} else {
+			// Invalid character
+			return 0;
+		}
 		i++;
-		len--;
 	}
 
-	return (sum);
+	// Check if there is an 'H' suffix and handle it if so
+	if (b[len] == 'H') {
+		// Check that all remaining characters are '0'
+		i = len + 1;
+		while (b[i] != '\0') {
+			if (b[i] != '0') {
+				// Invalid 'H' suffix
+				return 0;
+			}
+			i++;
+		}
+
+		// Handle the 'H' suffix
+		sum *= 10u;
+	}
+
+	return sum;
 }
