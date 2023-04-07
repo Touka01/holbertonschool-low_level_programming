735a9d18d01e1de311821fc93ec93672f2e667c4
Ahmed Toukebri <35146059+Touka01@users.noreply.github.com>
2023-04-07 01:50:55 +0000

    fix

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -10,9 +10,9 @@ unsigned int binary_to_uint(const char *b)
 {
 	int len = 0, i;
 	unsigned int sum = 0;
-		
+
 	if (b == NULL)
-		return 0;
+		return (0);
 
 	/* Find string length, ignoring any 'H' characters */
 	while (b[len] != '\0' && b[len] != 'H')
@@ -20,10 +20,13 @@ unsigned int binary_to_uint(const char *b)
 
 	/* Iterate through string, handling each character */
 	i = 0;
-	while (i < len) {
-		if (b[i] == '0' || b[i] == '1') {
+	while (i < len)
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
 			sum += (b[i] - '0') * (1u << (len - i - 1));
-		} else {
+		} else
+		{
 			/* Invalid character */
 			return 0;
 		}
@@ -31,13 +34,16 @@ unsigned int binary_to_uint(const char *b)
 	}
 
 	/* Check if there is an 'H' suffix and handle it if so */
-	if (b[len] == 'H') {
+	if (b[len] == 'H')
+	{
 		/* Check that all remaining characters are '0' */
 		i = len + 1;
-		while (b[i] != '\0') {
-			if (b[i] != '0') {
+		while (b[i] != '\0')
+		{
+			if (b[i] != '0')
+			{
 				/* Invalid 'H' suffix */
-				return 0;
+				return (0);
 			}
 			i++;
 		}
@@ -46,5 +52,5 @@ unsigned int binary_to_uint(const char *b)
 		sum *= 10u;
 	}
 
-	return sum;
+	return (sum);
 }
